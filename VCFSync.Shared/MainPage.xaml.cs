2de3b2abc7f2095ed9e2985f08b9082e2368fa1f
heman <heman@DESKTOP-JCV27O0>
2022-07-06 10:11:03 +0530

    Updates for first UI draft

--- VCFSync.Shared/MainPage.xaml.cs
+++ VCFSync.Shared/MainPage.xaml.cs
@@ -11,7 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Navigation;  
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
